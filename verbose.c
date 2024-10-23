21b1339d45e6d2c5ba1d375f788c46ca6d3008cf
rodxja <rodxja@gmail.com>
2024-10-22 19:41:46 -0600

    add logDebug

--- verbose.c
+++ verbose.c
@@ -4,6 +4,10 @@
 #include "verbose.h"
 
 int logLevel = 0;
+// 0 = just for logAll
+// 1 = logAll and logError
+// 2 = logAll, logError and logInfo
+// 3 = logAll, logError, logInfo and logDebug
 
 void logAll(const char *format, ...)
 {
@@ -33,6 +37,19 @@ void logError(const char *format, ...)
 void logInfo(const char *format, ...)
 {
     if (logLevel == 2)
+    {
+        va_list args;           // Declare a variable argument list
+        va_start(args, format); // Initialize the list with the last fixed parameter
+
+        vprintf(format, args); // Use vprintf to handle the variable arguments
+
+        va_end(args); // Clean up the variable argument list
+    }
+}
+
+void logDebug(const char *format, ...)
+{
+    if (logLevel == 3)
     {
         va_list args;           // Declare a variable argument list
         va_start(args, format); // Initialize the list with the last fixed parameter
